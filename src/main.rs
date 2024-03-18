7eedf4224c676c6ae577f1412dfe37ba8e80f3d1
kip2 <tqa1no4@gmail.com>
2024-03-18 21:33:16 +0900

    Connect to DB and get table value

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+use dotenv::dotenv;
+use sqlx::mysql::MySqlPoolOptions;
 use std::env;
 
-use dotenv::dotenv;
+#[tokio::main]
+async fn main() {
+    // Read environment variables and generate URL
+    dotenv::dotenv().expect("Fialed to read .env file");
+    let database_url = env::var("DATABASE_URL").expect("DABASE_URL must be set");
 
-fn main() {
-    dotenv().ok();
-    println!("{}", env::var("DATABASE_URL").unwrap());
-    println!("Hello, world!");
+    // Generate DB connection
+    let pool = MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+        .unwrap_or_else(|_| panic!("Cannot connect to the database"));
+
+    // Execute SQL query
+    let rows = sqlx::query!("SELECT * FROM Cars")
+        .fetch_all(&pool)
+        .await
+        .expect("Failed to db fetch");
+
+    // Print result
+    for row in rows {
+        println!("Value: {:?}", row.model.unwrap());
+    }
 }
