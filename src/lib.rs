0004fd95e98a50786ca24449eeeb9810ed1170a7
kip2 <tqa1no4@gmail.com>
2024-03-23 18:54:46 +0900

    Modify to accept and execute fiel path

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,77 @@
-use clap::{Args, Parser};
-use std::error::Error;
+use clap::Parser;
+use sqlx::mysql::MySqlPoolOptions;
+use sqlx::{MySql, Pool};
+use std::{env, error::Error, fs};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 pub struct Config {
-    #[arg(short = 'f', long = "filepath", help = "Input file path")]
-    filepath: String,
+    #[arg(short = 'f', long = "file", help = "Input file path")]
+    file: String,
 }
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn get_args() -> MyResult<Config> {
-    let args = Config::parse();
-    Ok(args)
+    let config = Config::parse();
+    Ok(config)
+}
+
+pub async fn run(config: Config) -> MyResult<()> {
+    // Read environment variables and generate URL
+    dotenv::dotenv().expect("Fialed to read .env file");
+    let database_url = env::var("DATABASE_URL").expect("DABASE_URL must be set");
+
+    // Generate DB connection
+    let pool = MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+        .unwrap_or_else(|_| panic!("Cannot connect to the database"));
+
+    // SQL query file path
+    let path = config.file;
+
+    // Read SQL queries
+    let queries = read_sql_file(&path).unwrap();
+
+    execute_query(&pool, queries).await;
+    Ok(())
+}
+
+fn read_sql_file(path: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let contents = fs::read_to_string(path)?;
+
+    let queries = contents
+        .split(';')
+        .filter(|s| !s.trim().is_empty())
+        .map(|s| s.trim().to_string())
+        .collect();
+    Ok(queries)
+}
+
+async fn execute_query(db: &Pool<MySql>, queries: Vec<String>) {
+    // Gererate transaction
+    let mut tx = db.begin().await.expect("transaction error.");
+
+    for query in queries {
+        // Execute SQL query
+        let result = sqlx::query(&query).execute(&mut *tx).await;
+
+        match result {
+            Ok(_) => {}
+            Err(e) => {
+                println!("Database query failed: {}", e);
+                println!("Failed query: {:?}", &query);
+                // rollback
+                tx.rollback().await.expect("Transaction rollback error.");
+                return;
+            }
+        }
+    }
+
+    // transaction commit
+    let _ = tx.commit().await.unwrap_or_else(|e| {
+        println!("{:?}", e);
+    });
 }
